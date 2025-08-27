9853fb00c63ca7f0d502a094c1a5e5af87bae69e
isaacsaless <isaacnascimentosales@gmail.com>
2025-08-27 16:52:03 -0300

    fix: temporary fix for get-data route

--- src/routes/get-data.ts
+++ src/routes/get-data.ts
@@ -5,6 +5,8 @@ import { parse } from "csv-parse/sync";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import fs from "fs";
+import path from "path";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -17,24 +19,26 @@ export default async function routes(fastify: FastifyInstance) {
         return reply.status(500).send("API key is not configured");
       }
       try {
-        const dataAtual = dayjs().tz("America/Sao_Paulo").format("YYYY-MM-DD");
-        const response = await axios.get(
-          `https://firms.modaps.eosdis.nasa.gov/api/country/csv/${process.env.NASA_API_KEY}/VIIRS_SNPP_NRT/BRA/1/${dataAtual}`
-        );
+        // const dataAtual = dayjs().tz("America/Sao_Paulo").format("YYYY-MM-DD");
+        // const response = await axios.get(
+        //   `https://firms.modaps.eosdis.nasa.gov/api/country/csv/${process.env.NASA_API_KEY}/VIIRS_SNPP_NRT/BRA/1/${dataAtual}`
+        // );
 
-        if (response.status !== 200) {
-          return reply
-            .status(response.status)
-            .send("Failed to fetch data from NASA FIRMS");
-        }
+        // if (response.status !== 200) {
+        //   return reply
+        //     .status(response.status)
+        //     .send("Failed to fetch data from NASA FIRMS");
+        // }
 
-        const responseData = await response.data;
-        const records = parse(responseData, {
+        // const responseData = await response.data;
+
+        const csvPath = path.join(process.cwd(), "src", "dados.csv");
+        const csvData = fs.readFileSync(csvPath, "utf-8");
+
+        const records = parse(csvData , {
           columns: [
-            null,
             "latitude",
             "longitude",
-            null,
             null,
             null,
             null,
